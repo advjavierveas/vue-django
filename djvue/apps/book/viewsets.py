aa967ce12405149ef4b15163d3dccff7961ebe67
advjavierveas <advjavierveas@gmail.com>
2019-04-03 00:56:49 -0500

    subiendo prooyecto django integrado con Vue.js

---
+++ djvue/apps/book/viewsets.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from .serializer import BookSerializer
+from .models import Book
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
